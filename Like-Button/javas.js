f3681fe0b1db9d50e23674f782f013610640fd9f
Soni kumari <Sonikumari78707@gmail.com>
2024-12-15 00:11:26 +0530

    completed

--- Like-Button/javas.js
+++ Like-Button/javas.js
@@ -5,11 +5,12 @@ con.addEventListener("dblclick",function(){
 	love.style.transform='translate(-50%,-50%) scale(1)'
 	love.style.opacity=0.8
 
+	// heart small size
 	setTimeout(function(){
 		love.style.transform='translate(-50%,-50%) scale(0)'
-		
 	},2000)
 
+	// not small 
 	setTimeout(function(){
 		love.style.opacity=0
 	},1000)
