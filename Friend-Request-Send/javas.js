f0cb6ec648036c67f323250799ab9d52f48bcc67
Soni kumari <Sonikumari78707@gmail.com>
2024-12-14 21:41:46 +0530

    completed

---
+++ Friend-Request-Send/javas.js
@@ -0,0 +1,30 @@
+let istatus=document.querySelector("h5")
+let btn=document.querySelector("#add")
+var check=0;
+
+// let remove=document.querySelector("#remove")
+// remove.addEventListener("click",function(){
+// 	istatus.innerHTML="Stranger";
+// 	istatus.style.color="red"
+// })
+
+// btn.addEventListener("click",function(){
+//    istatus.innerHTML="Friends";
+// 	 istatus.style.color="green"
+// })
+
+btn.addEventListener("click",function(){
+	if(check==0){
+       	istatus.innerHTML="Friends😊";
+	      istatus.style.color="green";
+				btn.innerHTML="Remove Friends";
+				check=1
+	}
+	else{
+		   istatus.innerHTML="Stranger";
+			istatus.style.color="red"
+			btn.innerHTML="Add Friends";
+			 check=0
+	}
+
+})
